8dc1430e495e810566fe98ba53fe1bbeb77ad2b1
jladvual <jladuval@gmail.com>
2013-07-29 16:24:26 +1200

    added dinner reader and started on geospacial queries

---
+++ Accounts/Readers/DinnerReader.cs
@@ -0,0 +1,30 @@
+﻿namespace Accounts.Readers
+{
+    using Interfaces.Presentation.Dinner;
+    using Interfaces.Readers;
+    using NetTopologySuite.Geometries;
+    using NHibernate;
+    using NHibernate.Spatial.Type;
+
+    public class DinnerReader : IDinnerReader
+    {
+        private readonly ISession _session;
+
+        public DinnerReader(ISession session)
+        {
+            _session = session;
+        }
+
+        public DinnerListDto GetDinnerList(double lat, double lng)
+        {
+            var point = new Point(lat, lng);
+            var result = _session.CreateQuery(@"select p from Dinner p 
+                                   order by NHSP.Distance(p.Location.GeoLoc, :point)")
+                    .SetParameter("point", point,
+                        NHibernateUtil.Custom(typeof(MsSql2008GeometryType)))
+                    .SetMaxResults(10)
+                    .List();
+            return new DinnerListDto();
+        }
+    }
+}
