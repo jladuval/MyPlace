ee6bb591ceb3b973089fab046d373388100f2f4e
jladvual <jladuval@gmail.com>
2013-09-03 08:02:50 +1200

    added age migration. renamed migrations to use xxx scheme

---
+++ Infrastructure.Data/Migrations/010_AddAgeToUser.cs
@@ -0,0 +1,21 @@
+﻿namespace Infrastructure.Data.Migrations
+{
+    using FluentMigrator;
+
+    [Migration(201309030755)]
+    public class AddAgeToUser : Migration
+    {
+        public override void Up()
+        {
+            Alter.Table("Users")
+                .AddColumn("Age")
+                    .AsInt32()
+                    .Nullable();
+        }
+
+        public override void Down()
+        {
+            Delete.Column("Age").FromTable("Users");
+        }
+    }
+}
