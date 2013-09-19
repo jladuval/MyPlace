69c69a436432b27617c2de2dffa414935085e77a
jladvual <jladuval@gmail.com>
2013-09-19 21:51:59 +1200

    Minor styling to review page

--- Accounts/Readers/Queries/Dinner/GetDinnerForReviewQuery.cs
+++ Accounts/Readers/Queries/Dinner/GetDinnerForReviewQuery.cs
@@ -39,9 +39,9 @@
                             Name = y.User.FullName(),
                             ApplicantId = y.User.Id,
                             GenderOrientation =
-                                String.Format("{0} {1}", y.User.Gender.ToString(), y.User.Orientation.ToString()),
+                                String.Format("{0}, {1}", y.User.Gender.ToString(), y.User.Orientation.ToString()),
                             PartnerGenderOrientation = y.Partner == null ? null :
-                                String.Format("{0} {1}", y.Partner.Gender.ToString(), y.Partner.Orientation.ToString()),
+                                String.Format("{0}, {1}", y.Partner.Gender.ToString(), y.Partner.Orientation.ToString()),
                             PartnerId = y.Partner == null ? null : (Guid?)y.Partner.Id,
                             PartnerName = y.Partner == null ? null : y.Partner.FullName()
                         }).ToList()
